b7d67348d08bff3228ae291628fc30dc9db282ca
caiolobo <caiolobo09@gmail.com>
2024-03-24 20:02:50 -0300

    Finalizando os Models e configurando o Settings

--- contact/admin.py
+++ contact/admin.py
@@ -4,11 +4,17 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'owner', 'category',
     ordering = '-id',
-    # list_filter = 'first_name',
+    list_filter = 'category',
     search_fields = 'first_name', 'phone',
-    list_per_page = 5 #quantas linhas serão exibidas
+    list_per_page = 30 #quantas linhas serão exibidas
     list_max_show_all = 30 #quantidade máxima de linhas que serão exibidas caso clicar em mostrar tudo
     #list_editable = 'phone', 'email', permite editar informações vinculadas nessa tupla
     list_display_links = 'first_name', 'last_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+    
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
@@ -10,7 +26,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default =True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
-    
+    category = models.ForeignKey(Category, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True) 
+    owner = models.ForeignKey(User, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True) 
 
 
     def __str__(self) -> str:
