0f2e917456c599d6a025c7fff383d9916e3a6451
caiolobo <caiolobo09@gmail.com>
2024-03-26 00:00:05 -0300

    Criando as Views do projeto e configurando os templates estáticos

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'owner', 'category',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'owner', 'category','show',
     ordering = '-id',
     list_filter = 'category',
     search_fields = 'first_name', 'phone',
     list_per_page = 30 #quantas linhas serão exibidas
     list_max_show_all = 30 #quantidade máxima de linhas que serão exibidas caso clicar em mostrar tudo
-    #list_editable = 'phone', 'email', permite editar informações vinculadas nessa tupla
+    list_editable = 'phone', 'show', #permite editar informações vinculadas nessa tupla
     list_display_links = 'first_name', 'last_name',
 
 @admin.register(models.Category)
