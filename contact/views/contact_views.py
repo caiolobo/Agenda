eb6c8312d0a1b745e88fc769ac1c1ee7a555571d
caiolobo <caiolobo09@gmail.com>
2024-03-28 00:15:21 -0300

    Finalizando a paginação do projeto

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -39,9 +39,15 @@ def search(request):
             Q(email__icontains=search_value)
             )\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 20)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search -'
     }
 
